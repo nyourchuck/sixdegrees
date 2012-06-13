f12f4f2c7e94b342bcdaecee3e8dafdae46ecfc1
Nate Yourchuck <nate@nimblegorilla.com>
2012-06-13 18:55:27 -0500

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixdegrees::Application.config.secret_token = 'df5db37fe52a4ece6175483d1f8ea106fe8e7bb025e69c5c96295b0239ffbef792c7ad78e0cf4b65497e5d5eeca8c6449fbc809a1e81c1a2811091371e7e9227'
